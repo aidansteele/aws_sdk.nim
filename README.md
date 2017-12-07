1282298dba85e925662140947c2b61e43247c075
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-12-07 02:19:08 +0200

    More additions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# aws_sdk
+# aws_sdk [![Build Status](https://travis-ci.org/aidansteele/aws_sdk.nim.svg?branch=master)](https://travis-ci.org/aidansteele/aws_sdk.nim)
 
 `aws_sdk` is a Nim library for interacting with [Amazon Web Services][aws].
 
@@ -8,11 +8,11 @@
 
 `aws_sdk` is still a very much work in progress. Right now it is just the
 bare essentials: request signing. All AWS operations require authenticated
-HTTP calls - this library calculates the necessary header values. 
+HTTP calls - this library calculates the necessary header values.
 
 It is planned to incorporate a library of functions that cover all the services
 provided by AWS - making the most of Nim's rich type-system and language
-features. 
+features.
 
 ## Usage
 
@@ -22,21 +22,21 @@ import aws_sdk/request
 import aws_sdk/http
 
 let payload = ""
-let payloadHash = sphHash[SHA256](payload) 
+let payloadHash = sphHash[SHA256](payload)
 # special header required by S3
 let headers = toTable({ "x-amz-content-sha256": hexify(payloadHash) })
 
-# T in AwsRequest[T] can also be a StringTableRef 
+# T in AwsRequest[T] can also be a StringTableRef
 let req = AwsRequest[Table[string, string]](
-  httpMethod: "GET", 
-  uri: parseUri("https://s3.amazonaws.com/"), 
-  headers: headers, 
+  httpMethod: "GET",
+  uri: parseUri("https://s3.amazonaws.com/"),
+  headers: headers,
   payloadHash: payloadHash
 )
 
 # initAwsCredentialsFromEnv(): AwsCredentials is also available
 let credentials = AwsCredentials(
-  accessKeyId: "AKIDEXAMPLE", 
+  accessKeyId: "AKIDEXAMPLE",
   secretKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY"
 )
 
--- aws_sdk/utils/client.nim
+++ aws_sdk/utils/client.nim
@@ -14,13 +14,20 @@ type
         endpointPrefix*: string
         targetPrefix*: string # Target prefix for json protocol
         jsonVersion*: string
+        apiVersion*: string
+        signingName*: string
 
 template defineClient*(name: untyped) =
     type name* = ref object of Client
     proc fillMetadata*(c: name) =
-        c.targetPrefix = awsApiMD_targetPrefix
         c.endpointPrefix = awsApiMD_endpointPrefix
-        c.jsonVersion = awsApiMD_jsonVersion
+        c.apiVersion = awsApiMD_apiVersion
+        when declared(awsApiMD_targetPrefix):
+            c.targetPrefix = awsApiMD_targetPrefix
+        when declared(awsApiMD_jsonVersion):
+            c.jsonVersion = awsApiMD_jsonVersion
+        when declared(awsApiMD_signingName):
+            c.signingName = awsApiMD_signingName
 
 proc init(c: Client, credentials: AwsCredentials, region: string) =
     c.credentials = credentials
@@ -47,7 +54,9 @@ proc request*(c: Client, req: AwsRequest, content: string = ""): Future[string] 
     if "Date" notin req.headers and "X-Amz-Date" notin req.headers:
         req.headers["Date"] = format(getGMTime(time), HttpDateFormat)
 
-    let scope = initCredentialScope(req.uri, time)
+    var scope = initCredentialScope(req.uri, time)
+    if not c.signingName.isNil:
+        scope.service = c.signingName
     req.headers["Authorization"] = authorizationHeaderv4(c.credentials, scope, req)
 
     if c.cl.isNil:
@@ -87,3 +96,34 @@ proc sendJsonRequest*(c: Client, name, httpMethod, uri: string, r: JsonNode): Fu
 
     let resp = await c.request(req, payload)
     result = parseJson(resp)
+
+when false: # Not ready yet
+    proc sendEC2Request*(c: Client, name, httpMethod, uri: string, r: JsonNode): Future[JsonNode] {.async.} =
+        const HttpDateFormat = "yyyyMMdd'T'HHmmss'Z'"
+        #const HttpDateFormat = "ddd, dd MMM yyyy HH:mm:ss 'UTC'"
+        let time = getTime()
+        let timeStr = format(getGMTime(time), HttpDateFormat)
+
+        let payload = ""
+        let payloadHash = sphHash[SHA256](payload)
+
+        # special header required by S3
+        let headers = newStringTable({
+            "content-type": "application/x-www-form-urlencoded",
+            "x-amz-date": timeStr,
+            }, modeCaseInsensitive)
+
+        var uri = c.endpoint & "?Action=" & name & "&Version=" & c.apiVersion
+
+        # appendParamsToQuery()
+
+        let req = AwsRequest[StringTableRef](
+            httpMethod: httpMethod,
+            uri: parseUri(uri),
+            headers: headers,
+            payloadHash: payloadHash
+        )
+
+        let resp = await c.request(req, payload)
+        echo "RESP: ", resp
+        result = parseJson(resp)
